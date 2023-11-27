5975ce2d714b007e192b337bb4b22b3ac594a0a6
belovroman90 <belov.roman.90@mail.ru>
2023-11-27 08:55:39 +0700

    Completed HW11 task

--- src/s2-homeworks/hw05/pages/JuniorPlus.tsx
+++ src/s2-homeworks/hw05/pages/JuniorPlus.tsx
@@ -9,8 +9,8 @@ import HW11 from '../../hw11/HW11'
 function JuniorPlus() {
     return (
         <div id={'hw5-page-junior-plus'}>
-            <HW10/>
-            {/*<HW11/>*/}
+            {/*<HW10/>*/}
+            <HW11/>
             {/*<HW12 />*/}
             {/*<HW13 />*/}
             {/*<HW14 />*/}
