e983614ccdeec53d992d609cdeeacce9dba8dfdb
belovroman90 <belov.roman.90@mail.ru>
2024-01-19 14:34:59 +0700

    Change type of option

--- src/s2-homeworks/hw07/common/c5-SuperSelect/SuperSelect.tsx
+++ src/s2-homeworks/hw07/common/c5-SuperSelect/SuperSelect.tsx
@@ -12,14 +12,12 @@ type DefaultSelectPropsType = DetailedHTMLProps<
 
 type SuperSelectPropsType = DefaultSelectPropsType & {
     options?: any[]
-    onChangeOption?: (option: any) => void
+    onChangeOption?: (option: number) => void
 }
 
 const SuperSelect: React.FC<SuperSelectPropsType> = ({
                                                          options,
-                                                         className,
-                                                         onChange,
-                                                         onChangeOption,
+                                                         className, onChange, onChangeOption,
                                                          ...restProps
                                                      }) => {
 
@@ -39,7 +37,7 @@ const SuperSelect: React.FC<SuperSelectPropsType> = ({
     const onChangeCallback = (e: ChangeEvent<HTMLSelectElement>) => {
         // делают студенты
         if (onChangeOption) {
-            onChangeOption(e.target.value)
+            onChangeOption(+e.target.value)
         }
     }
 
